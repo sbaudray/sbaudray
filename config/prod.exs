9a335e411b94d66a7c9769fddf465adeb6629252
sbaudray <baudray.sebastien@gmail.com>
2020-05-12 17:33:50 +0100

    correct host

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :sbaudray, SbaudrayWeb.Endpoint,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  url: [host: "example.com", port: 80],
+  url: [host: "sbaudray.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
